f290adf997a2b3fcd215c3fc5cc39a01d96a007f
unknown <Administrador@A112LSL.tcpsi.es>
2018-10-24 16:46:21 +0200

    Add calendar component to test spec

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,31 +1,37 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { CalendarComponent } from './calendar/calendar.component';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
 describe('AppComponent', () => {
+  let component: AppComponent,
+      fixture: ComponentFixture<AppComponent>,
+      el: DebugElement;
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent, CalendarComponent
       ],
     }).compileComponents();
   }));
 
-  it('should create the app', () => {
+  beforeEach(()=>{
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.debugElement.componentInstance;
+    el = fixture.debugElement;
+  })
+
+  it('should create the app', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
-  });
+  }));
 
-  it(`should have as title 'mini-calendar'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('mini-calendar');
-  });
+  it('should include the mini calendar inside the app', async(() => {
+    const calendarComponent = el.query(By.directive(CalendarComponent));
+    expect(calendarComponent).toBeTruthy();
+    console.log(calendarComponent.nativeElement.outerHTML);
+  }));
 
-  it('should render title in a h1 tag', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to mini-calendar!');
-  });
 });
