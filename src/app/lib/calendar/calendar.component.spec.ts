7183f93bd52a08188cf0120c93820e4c778c4b46
unknown <Administrador@A112LSL.tcpsi.es>
2018-10-25 11:12:45 +0200

    test calendar component

--- src/app/lib/calendar/calendar.component.spec.ts
+++ src/app/lib/calendar/calendar.component.spec.ts
@@ -19,7 +19,29 @@ describe('CalendarComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create Calendar component', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should set currentDay correctly', () => {
+    expect(component.currentDate.toString()).toBe(new Date().toString(), 'Show today at first');
+  });
+
+  it('should show the next month correctly', () => {
+    component.currentDate = new Date('October 22, 2018 00:00:00');
+    component.getNextMonth();
+    expect(component.currentDate.toDateString()).toBe('Thu Nov 01 2018', 'Show next month');
+    component.currentDate = new Date('December 22, 2018 00:00:00');
+    component.getNextMonth();
+    expect(component.currentDate.toDateString()).toBe('Tue Jan 01 2019', 'Show next month which is next year as well');
+  });
+
+  it('should show the previous month correctly', () => {
+    component.currentDate = new Date('October 22, 2018 00:00:00');
+    component.getPreviousMonth();
+    expect(component.currentDate.toDateString()).toBe('Sat Sep 01 2018', 'Show previous month');
+    component.currentDate = new Date('January 22, 2018 00:00:00');
+    component.getPreviousMonth();
+    expect(component.currentDate.toDateString()).toBe('Fri Dec 01 2017', 'Show previous month which is previous year as well');
   });
 });
